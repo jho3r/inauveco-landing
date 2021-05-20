10e82825f8603a48aee18a9fcec7480bb1655741
Jhoer <jhoer.perez@gmail.com>
2021-05-20 12:17:19 -0500

    Eliminando el spinner

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,7 @@
-$("section").hide();
-$(window).on("load", function() {
-    $('.spiner').addClass("hide");
-    $('.spiner').hide();
-    $('section').show();
-    //$('.spinner-border').hide();
-});
+// $("section").hide();
+// $(window).on("load", function() {
+//     $('.spiner').addClass("hide");
+//     $('.spiner').hide();
+//     $('section').show();
+//     //$('.spinner-border').hide();
+// });
